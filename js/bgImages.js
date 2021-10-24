2cb10a93588da52304d7b17485006ff8a3d32dc9
Isaac Sarte <aizakku09@gmail.com>
2021-10-24 19:57:41 +0800

    phone query done

--- js/bgImages.js
+++ js/bgImages.js
@@ -1,6 +1,6 @@
 let images = [
     "url('https://images.pexels.com/photos/1229861/pexels-photo-1229861.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')",
-    "url('https://images.pexels.com/photos/967016/pexels-photo-967016.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')",
+    "url('https://images.pexels.com/photos/414645/pexels-photo-414645.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')",
     "url('https://images.pexels.com/photos/97987/pexels-photo-97987.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')",
     "url('https://images.pexels.com/photos/163143/sackcloth-sackcloth-textured-laptop-ipad-163143.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')",
     "url('https://images.pexels.com/photos/3184464/pexels-photo-3184464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')",
